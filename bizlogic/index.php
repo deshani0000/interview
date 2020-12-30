a4be07556355d8659a541e36faf94b19b916ca10
deshani0000 <68660006+deshani0000@users.noreply.github.com>
2020-12-30 11:31:09 +0530

    Add files via upload

---
+++ bizlogic/index.php
@@ -0,0 +1,31 @@
+<?php 
+
+$type = $_GET['tp']; 
+elseif($type=='login') login(); 
+
+ 
+function login() 
+{ 
+       require 'config.php'; 
+       $json = json_decode(file_get_contents('php://input'), true); 
+       $username = $json['username']; $password = $json['password']; 
+       $userData =''; $query = "select * from users where username='$username' and password='$password'"; 
+       $result= $db->query($query);
+       $rowCount=$result->num_rows;
+             
+        if($rowCount>0)
+        {
+            $userData = $result->fetch_object();
+            $user_id=$userData->user_id;
+            $userData = json_encode($userData);
+            echo '{"userData":'.$userData.'}';
+
+            
+        }
+        else 
+        {
+            echo '{"error":"Wrong username and password"}';
+        }
+
+    
+}
